18efd4c62a549124e29c58013bbc0bd13dccefe5
Aviator 2277 <91645065+Aviator2276@users.noreply.github.com>
2024-03-19 12:36:55 -0400

    Changed auto to auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -79,7 +79,7 @@ public class RobotContainer {
   private final GenericEntry alliancebox = autoTab.add("Red Alliance", false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
   private final SendableChooser<File> fileChooser = new SendableChooser<>();
   // Playback
-  private PlayBack playBackAuto;
+  //private PlayBack playBackAuto;
   //private AUTO_Playback AUTO_Playback;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -134,7 +134,7 @@ public class RobotContainer {
     m_operatorController.povDownRight().whileTrue(driveClimbDown);
 
     // DRIVER BUTTONS
-    playBackAuto = new PlayBack(m_robotDrive, onboarder, shooter, fileChooser, alliancebox);
+    //playBackAuto = new PlayBack(m_robotDrive, onboarder, shooter, fileChooser, alliancebox);
     new JoystickButton(leftStick, 6).onTrue(cancelAll);
     new JoystickButton(leftStick, 7).onTrue(cancelAll);
     // Disabled to prevent accidentally playback. If testing, use autonomous period of FRC Drive Station.
@@ -144,7 +144,7 @@ public class RobotContainer {
     new JoystickButton(rightStick, 1).whileTrue(new OnboarderSystem(onboarder, m_operatorController, true, false));
     new JoystickButton(rightStick, 3).whileTrue(driveShooter);
 
-    // Lambdas that don't need a command.
+    // Lambdas that don't need a command
     new JoystickButton(leftStick, 10).whileTrue(new RunCommand(() -> {
       m_robotDrive.resetGyro();
     }));
@@ -172,7 +172,7 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // Run currently selected playback file
-    //return new AUTO_Playback(ampSystem, m_robotDrive, onboarder, shooter, fileChooser, alliancebox);
+    //return new Auton_Playback(ampSystem, m_robotDrive, onboarder, shooter, fileChooser, alliancebox);
     return new PlayBack(m_robotDrive, onboarder, shooter, fileChooser, alliancebox);
   }
 }
